0ef8191a367513484b0d810a17553b62020ba04f
jonmolinna <jmolina2624@gmail.com>
2023-08-10 05:51:56 -0500

    Strings

---
+++ 1-basico/3-strings.py
@@ -0,0 +1,18 @@
+# Strings
+name, surname, age = "Kendra", "Contreras", 27
+print('Mi name es {} {} y mi edad es {}'.format(name, surname, age))
+print(f"Mi nombre es {name} {surname} y mi edad es {age}")
+
+# Slice
+name = "kendra contreras"
+name_slice = name[-2]
+print(name_slice)  # a
+
+# Reverse
+reverse_name = name[::-1]
+print(reverse_name)  # sarertnoC ardneK
+
+# Funciones
+print(name.capitalize())
+print(name.upper())
+print(name.count('e'))  # 2
