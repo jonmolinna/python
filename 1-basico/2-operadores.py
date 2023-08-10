1f68a97c15f98766b8840d6efd2713e8e9b468fd
jonmolinna <jmolina2624@gmail.com>
2023-08-10 05:51:36 -0500

    Operadores

---
+++ 1-basico/2-operadores.py
@@ -0,0 +1,20 @@
+# Operadores Aritmeticos
+print(3 + 4)
+print(3 - 4)
+print(3 * 4)
+print(10 % 2)
+print(10 // 3)  # 3
+print(2 ** 3)  # 8
+
+# Operadores Comparativas
+print(3 > 4)
+print(3 < 4)
+print(3 >= 4)
+print(3 <= 4)
+print(3 == 4)
+print(3 != 4)
+
+# Operadores Logicos
+# and
+# or
+# not
