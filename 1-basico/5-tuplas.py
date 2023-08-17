e42c5531302f1f6c59c7474029266eab5cc3d6d4
jonmolinna <jmolina2624@gmail.com>
2023-08-17 06:53:53 -0500

    Tuplas

---
+++ 1-basico/5-tuplas.py
@@ -0,0 +1,11 @@
+# Tuple
+my_tuple = tuple()
+my_other_tuple = ()
+
+my_tuple = (27, 1.69, "Kendra", "Contreras")
+print(my_tuple)
+
+# Convierte una tuple a lista
+my_list = list(my_tuple)
+
+print(my_list)
