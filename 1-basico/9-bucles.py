a0f99e8066cb2b78157218fbc417f345720237f6
jonmolinna <jmolina2624@gmail.com>
2023-08-31 14:58:09 -0500

    Bucles

---
+++ 1-basico/9-bucles.py
@@ -0,0 +1,28 @@
+# Bucles
+
+# While
+my_condition = 0
+
+while my_condition < 10:
+    print(my_condition)
+    my_condition += 2
+
+print("===============")
+
+# For
+my_list = [2, 4, 6, 8, 10]
+
+for element in my_list:
+    print(element + 1)
+
+print("=================")
+
+my_dict = {
+    "name": "Kendra",
+    "last_name": "Contreras",
+    "age": 24,
+    "company": "Dallas"
+}
+
+for element in my_dict.values():
+    print(element)
