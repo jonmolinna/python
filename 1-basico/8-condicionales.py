1ebd96a2c7912418fda6603097974617771e44b9
jonmolinna <jmolina2624@gmail.com>
2023-08-31 14:57:51 -0500

    Condiciones

---
+++ 1-basico/8-condicionales.py
@@ -0,0 +1,14 @@
+# Condicionales
+my_condicion = True
+
+if my_condicion:
+    print('Se ejecuta la condicion')
+else:
+    print('La ejecucion continua')
+
+my_string = "Kendra"
+
+if my_string:
+    print("Existe un dato")
+else:
+    print("No existe dato")
