8bd3b1cfeb789e01245aabee3b1c114d7a099f37
jonmolinna <jmolina2624@gmail.com>
2023-09-07 16:48:44 -0500

    Modules

---
+++ 1-basico/94-modules.py
@@ -0,0 +1,10 @@
+# Modules
+# import modules
+
+from modules import resNumber
+
+# result = modules.sumarNumber(12, 15)
+# print(result)
+
+result = resNumber(12, 10)
+print(result)
---
+++ 1-basico/modules.py
@@ -0,0 +1,6 @@
+def sumarNumber(a, b):
+    return a + b
+
+
+def resNumber(a, b):
+    return a - b
