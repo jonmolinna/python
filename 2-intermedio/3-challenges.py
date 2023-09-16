27b91d554c7227434f5bc56b73af979bfe8ebd5a
jonmolinna <jmolina2624@gmail.com>
2023-09-16 05:52:08 -0500

    Challenge 4

--- 2-intermedio/3-challenges.py
+++ 2-intermedio/3-challenges.py
@@ -37,13 +37,74 @@ segun sean o no anagramas.
 """
 
 
-def anagrama(str1, str2):
-    array1 = list(str1.lower())
-    array1.sort()
-    array2 = list(str2.lower())
-    array2.sort()
-    print(array1)
-    print(array2)
+def is_anagram(str1, str2):
+    if str1.lower() == str2.lower():
+        return False
+    return sorted(str1.lower()) == sorted(str2.lower())
 
 
-anagrama("Zara", "Raza")
+# result = is_anagram("Roma", "Amor")
+# print(result)
+
+# Challenge 3
+"""
+Escrible un programa que imprima los 50 primeros numeros de la sucesion de fibonacci empezando 0.
+    - La serie fibonacci se compone por la sucesion de numeros en la que el siguiente siempre
+      es la suma de los dos anteriores.
+      ejm 0, 1, 1, 2, 3, 5, 8, 13 ....
+"""
+
+
+def fibonacci():
+    prev = 0
+    next = 1
+
+    for index in range(0, 51):
+        print(prev)
+        fib = prev + next
+        prev = next
+        next = fib
+
+
+# fibonacci()
+
+# Challengue 4
+"""
+Escribe un programa que se encarga de comprobar si un numero es o no primo.
+Hecho esto, imprime los numeros primos entre 1 y 100
+"""
+
+
+def is_primo(number):
+    if (number == 1):
+        return False
+
+    for i in range(2, number):
+        if (number % i == 0):
+            return False
+
+    return True
+
+
+def list_primos():
+    for index in range(1, 101):
+        result = is_primo(index)
+        if (result):
+            print(index, "Es primo")
+        else:
+            print(index)
+
+
+list_primos()
+
+
+# Otra forma
+def is_prime(number):
+    if number < 2:
+        return False
+
+    for index in range(2, 101):
+        if number % index == 0:
+            return False
+
+    return True
