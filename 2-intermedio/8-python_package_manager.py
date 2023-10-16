9fb0a5124121004545f779379a57b61201291c2b
jonmolinna <jmolina2624@gmail.com>
2023-10-16 06:24:40 -0500

    Package

---
+++ 2-intermedio/8-python_package_manager.py
@@ -0,0 +1,26 @@
+# https://pypi.org/project/pip/
+
+# pip --version
+# pip install pip
+# pip install --upgrade pip
+# pip
+
+# pip install numpy
+# pip install pandas
+
+# pip list
+# pip uninstall pandas
+
+from mypackage import aritmetrica
+import numpy
+
+# print(numpy.version.version)
+
+numpy_array = numpy.array([35, 24, 60, 52, 30, 30, 17])
+print(type(numpy_array))
+
+print(numpy_array * 2)
+
+# import mypackage
+
+print(aritmetrica.sum_two_number(1, 5))
