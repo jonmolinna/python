47ba97558eb1072107d2773f4763132ec4c79bac
jonmolinna <jmolina2624@gmail.com>
2024-05-31 15:02:53 -0500

    Operadores

---
+++ 1-basico/1-operadores.py
@@ -0,0 +1,22 @@
+# Ejercicio 1
+res = 8 - 6 / 2 + 1 * 2
+print(res)
+
+# Ejercicio 2
+# Tenemos 2 cajones de manzana  con 20 manzanas cada uno. Nos traen otros 3 cajones de manzanas
+# con 10 manzanas cada uno, ¿Cuantas manzanas tendresmos?
+
+res = 2 * 20 + 3 * 10
+print(res)
+
+# Ejercicio 3
+# Tenemos 2 cajones de manzanas y nos traen otros 3 cajones. Si cada cajon tiene 20 manzanas
+# ¿Cuantas manzanas tendremos?
+res = 20 * (2 + 3)
+print(res)
+
+# Ejercicio 4
+# Hay que repartir una caja de bombones de 24 bombones entre 2 grupos de 3 alumnos de cada uno.
+# ¿A cuantos bombones toca cada alumno?
+res = (24 / 2) / 3
+print(res)
---
+++ 1-basico/2-operadores.py
@@ -0,0 +1,56 @@
+# Ejercicio 1
+#  5 es menor o igual que 10
+# 5 es mayor o igual que 5
+print(5 <= 10)
+print(5 >= 5)
+
+# Ejercicio 2
+# 4 mas 3 es mayor que 6 menos 1
+# 3 por 5 es distinto que 5 por 3
+print(4 + 3 > 6 - 1)
+print(3 * 5 != 5 * 3)
+
+# Ejercicio 3
+# 2 mas 2 es igual a 4 y 5 es mayor que 3
+print( 2 + 2 == 4 and 5 > 3 )
+
+# Ejercicio 4
+# 2 mas 2 es igual a 4 y 5 es menor que 3
+print( 2 + 2 == 4 and 5 < 3)
+
+# Ejercicio 5
+# 5 es mayor que 7 and 3 es menor que 4
+# 8 es mayor o igual que 6 and 9 es diferente de 7
+print(5 > 7 and 3 < 4)
+print(8 >= 6 and 9 != 7)
+
+# Ejercicio 6
+# 2 mas 2 es igual a 4 o 5 es mayor que 3
+print(2 + 2 == 4 or 5 > 3)
+
+# Ejercicio 7
+# 2 mas 2 son 5 o 4 es igual a 4
+# 3 por 2 son 8 o 5 es distinto de 5
+print(2 + 2 == 5 or 4 == 4)
+print(3 * 2 == 8 or 5 != 5)
+
+# Ejercicio 8
+# No es que 6 sea mayor que 2
+# No es que 2 mas 2 sean 5
+print(not 6 > 2)
+print(not 2 + 2 == 5)
+
+# Ejercicio 9
+# No es que 3 mas 3 sea mayor que 2 por 3
+print(not 3 + 3 > 2 * 3)
+
+# Ejercicio 10
+# No es que 2 mas 2 sea igual a 5 o que 8 sea mayor que 3
+print(not 2 + 2 == 5 or 8 > 3)
+
+# Ejercicio 11
+# 5 por 2 es mayor que 9 and no es que 3 sea mayor que 4
+print(5 * 2 > 9 and not 3 > 4 )
+
+
+
